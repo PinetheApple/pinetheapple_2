3b044283270a007fcbe42391b57dbc28b790c9f4
PinetheApple <jonathansibypaul@gmail.com>
2022-07-13 11:11:10 +0530

    basic template on gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../utils/font-awesome'
 import {
   container,
   heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
+  pagelinks,
+  innerlinks,
+  leftsection,
+  rightsection,
+  line,
+  rotate90,
+  button,
 } from './layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
@@ -23,29 +29,34 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-        </ul>
+      <nav class={pagelinks}>
+          <Link to=''><StaticImage alt="Logo" src="../images/logo.svg"/></Link>
+          <ul class={innerlinks}>
+              <li><Link to=''>About</Link></li>
+              <li><Link to=''>Experience</Link></li>
+              <li><Link to='#projects'>Projects</Link></li>
+              <li><Link to='#contact'>Contact</Link></li>
+              <li><Link to='' className={button}>Resume</Link></li>
+          </ul>
       </nav>
+      {/* end of navigation */}
+      <nav class={leftsection}>
+            <ul>
+                <li><a href="https://www.discord.gg/rqVKvVe" target="_blank"><FontAwesomeIcon icon={['fab', 'discord']} size='xl' title='discord server link'/></a></li>
+                <li><a href="https://github.com/PinetheApple" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} size='xl' title='github profile link' /></a></li>
+                <li><a href="https://codepen.io/pinetheapple" target="_blank"><FontAwesomeIcon icon={['fab', 'codepen']} size='xl' title='github profile link' /></a></li>
+                <li><div class={line}></div></li>
+            </ul>
+      </nav>
+      {/* end of left section */}
+      <div class={rightsection}>
+            <p class={rotate90}><a href="mailto:">pinespace889@gmail.com</a></p>
+            <div class={line}></div>
+      </div>
+      {/* end of right section */}
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
+        {/* <h1 className={heading}>{pageTitle}</h1>
+        {children} */}
       </main>
     </div>
   )
---
+++ src/utils/font-awesome.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCodepen, faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
+library.add(faDiscord, faGithub, faCodepen)
+
+// fa-brands fa-discord fa-xl
